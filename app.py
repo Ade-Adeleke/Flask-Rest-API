231b3660aac6bb587670581c8c6dff49febcf9b8
Moshood Adekunjo <madekunjo@yahoo.com>
2022-12-24 23:25:53 +0100

    Improvements on the error handlings

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, jsonify, request,render_template
+from flask_smorest import abort
 from db import items,stores
 
 app = Flask(__name__)
@@ -14,6 +15,12 @@ def home():
 @app.post('/store')
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Ensure 'name' is in JSON Payload")
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"store already exist")
     store_id = uuid.uuid4().hex
 
     store = {** store_data, "id": store_id}
@@ -25,17 +32,33 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {'message': 'store not found'}
+        abort(404, message='store not found')
+
 
 @app.get('/store')
 def get_stores():
     return {'stores':list(stores.values())}
 
 @app.post('/item')
-def create_item(name):
+def create_item():
     item_data = request.get_json()
+    if (
+        "name" not in item_data
+        or "price" not in item_data
+        or "store_id" not in item_data
+    ):
+        abort(400,
+              message="Bad Request, Ensure 'name', 'price' and 'store_id' are in the Json Payload."
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message='Item already exists')
+
     if item_data["store_id"] not in stores:
-        return {"Store not found"}, 404
+        abort(404, message='store not found')
     item_id = uuid.uuid4().hex
 
     item = {**item_data, "id": item_id}
@@ -50,6 +73,6 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {'message': 'item not found'}, 404
+        abort(404, message='store not found')
 
 #app.run(debug=True)
--- db.py
+++ db.py
@@ -1,13 +1,3 @@
 stores = {}
 
-items = {
-    1: {
-        'name':'boot',
-        'price': 100.0
-    },
-
-    2: {
-        'name':'gloves',
-        'price': 90.0
-    }
-}
+items = {}
