c8ce5f8472c92c6c7c5169b92ce64bd18119f3e4
Moshood Adekunjo <madekunjo@yahoo.com>
2022-12-25 10:02:57 +0100

    Adding Update and Delete endpoints for Item and Store

--- app.py
+++ app.py
@@ -27,6 +27,26 @@ def create_store():
     stores[store_id]= store
     return store, 201
 
+@app.delete('/store/<string:store_id>')
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+        abort(400, message="Bad Request, Store not found")
+
+@app.put('/store/<string:store_id>')
+def update_store(store_id):
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad Request, Ensure 'name' is included")
+    try:
+        store = stores[store_id]
+        store |= store_data
+        return store
+    except KeyError:
+        abort(400, message="Bad Request, Store not found")
+
 @app.get('/store/<string:store_id>')
 def get_store(store_id):
     try:
@@ -74,5 +94,25 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         abort(404, message='store not found')
+
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message":"Item deleted"}
+    except KeyError:
+        abort(404, message='item not found')
+
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+    item_data = request.get_json()
+    if "name" not in item_data or "price" not in item_data:
+        abort(400, message="Bad Request, Ensure 'name' or 'price' is in Json Payload")
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    except KeyError:
+        abort(400, message="item not found")
 
 #app.run(debug=True)
