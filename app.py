34f3e65be3a01137d3962700571f17078eac251c
Moshood Adekunjo <madekunjo@yahoo.com>
2023-01-23 10:34:14 +0100

    Added register, get and delete endpoints for user module

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_smorest import Api
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 from db import db
 import models
 from flask_jwt_extended import JWTManager
@@ -26,7 +27,7 @@ def create_app(db_url = None):
 
     api = Api(app)
 
-    app.config("JWT_SECRET_KEY") = "91398425959890473578476272483330548806"
+    app.config["JWT_SECRET_KEY"] = "91398425959890473578476272483330548806"
     jwt = JWTManager(app)
 
     @app.before_first_request
@@ -36,5 +37,6 @@ def create_app(db_url = None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
--- resources/user.py
+++ resources/user.py
@@ -1,12 +1,12 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from passlib.has import pbkdf2_sha256
+from passlib.hash import pbkdf2_sha256
 
 from db import db
 from models import UserModel
 from schemas import UserSchema
 
-blp = Blueprint("Users", "users", descriprion="Operations on users")
+blp = Blueprint("Users", "users", description="Operations on users")
 
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -16,10 +16,23 @@ class UserRegister(MethodView):
             abort(409, message="Username has been taken")
 
         user = UserModel(
-            username=user_data["username"]
+            username=user_data["username"],
             password=pbkdf2_sha256.hash(user_data["password"])
         )
         db.session.add(user)
         db.session.commit()
 
         return {"message": "User created successful"}, 201
+
+    @blp.route("/user/<int:user_id>")
+    class User(MethodView):
+        @blp.response(200, UserSchema)
+        def get(self, user_id):
+            user = UserModel.query.get_or_404(user_id)
+            return user
+
+        def delete(self, user_id):
+            user = UserModel.query.get_or_404(user_id)
+            db.session.delete(user)
+            db.session.commit()
+            return {"message": "User deleted"}, 200
