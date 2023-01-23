80fe87afa8ee298f5efd9f18bdabfc532859be99
Moshood Adekunjo <madekunjo@yahoo.com>
2023-01-23 12:24:53 +0100

    Claims and authorization with jwt

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ def create_app(db_url = None):
     app.config["JWT_SECRET_KEY"] = "91398425959890473578476272483330548806"
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity==1:
+             return {"is_admin": True}
+        return {"is_admin": False}
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return(
