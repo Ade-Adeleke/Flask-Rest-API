82655daa9bb3c50edc7cb6ef3743015a12ee3187
Moshood Adekunjo <madekunjo@yahoo.com>
2023-02-21 12:37:04 +0100

    changes to plain item schema

--- schemas.py
+++ schemas.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 class PlainItemSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
-    description = fields.S
+    description = fields.Str(required=True)
     price = fields.Float(required=True)
 
 
